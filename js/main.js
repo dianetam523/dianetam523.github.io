06e52901c04df80804e9cebe2f04c25efc72c3e3
dianetam523 <diane.tam523@gmail.com>
2017-05-26 04:32:55 -0400

    Woooo new media design with flickr links, fading transitions, more content changes

--- js/main.js
+++ js/main.js
@@ -2,4 +2,51 @@ $("[data-collapse-group]").on('show.bs.collapse', function () {
   var $this = $(this);
   var thisCollapseAttr = $this.attr('data-collapse-group');
   $("[data-collapse-group='" + thisCollapseAttr + "']").not($this).collapse('hide');
+  $('.activeskill').removeClass('activeskill')
+  $(document.activeElement.getElementsByTagName("img")[0]).addClass('activeskill')
+
 });
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.navbar');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top +10) {
+          $('.navbar').css('background-color', 'transparent');
+       } else {
+          $('.navbar').css('background-color', 'rgba(34,34,34,0.9)');
+       }
+   });
+});
+
+// Add scrollspy to <body>
+$('body').scrollspy({target: ".navbar", offset: 50});
+
+// Add smooth scrolling on all links inside the navbar
+$("#myNavbar a").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } // End if
+
+});
+
